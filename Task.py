8756e2c3f3fafd631f4508eec41d2fb24d84ec8f
Kshitij Bajracharya <bajracharya.kshitij@gmail.com>
2018-12-19 23:23:51 +0545

    Add mysql connector

--- Task.py
+++ Task.py
@@ -1,3 +1,6 @@
+import mysql.connector
+from mysql.connector import errorcode
+
 class Task:
 	def __init__(self, title, description):
 		self.title = title
@@ -8,7 +11,25 @@ class Task:
 		print("Title: ", self.title)
 		print("Description: ", self.description)
 
+def handleDatabaseConnection():
+		try:
+			connection = mysql.connector.connect (
+				user = 'root',
+				password = 'root',
+				host = 'localhost',
+				database = 'taskmanager'
+			)
+			print("Database connected!")
+		except mysql.connector.Error as e:
+			if (e.errno == errorcode.ER_ACCESS_DENIED_ERROR):
+				print("Wrong user and/or password")
+			elif (e.errno == errorcode.ER_BAD_DB_ERROR):
+				print("Database doesn't exist")
+			else:
+				print(e)
+
 if __name__ == '__main__':
+	handleDatabaseConnection()
 	name = input("Enter task name: ")
 	description = input("Provide a description: ")
 
