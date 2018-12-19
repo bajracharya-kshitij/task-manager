c06cd75f15a6cd730b5081741d13888ae9d81409
Kshitij Bajracharya <bajracharya.kshitij@gmail.com>
2018-12-20 00:30:51 +0545

    Add possibility to save multiple tasks

--- DbHandler.py
+++ DbHandler.py
@@ -37,11 +37,10 @@ def getDatabaseConnection():
 	return connection
 
 def createTable(cursor):
-	print("Creating table...")
-	cursor.execute("DROP TABLE IF EXISTS task")
-
+	print("Setting up table...")
+	
 	sql_command = """
-		CREATE TABLE task (
+		CREATE TABLE IF NOT EXISTS task (
 			id INTEGER PRIMARY KEY,
 			title VARCHAR(20),
 			description VARCHAR(20)
@@ -49,19 +48,28 @@ def createTable(cursor):
 	"""
 
 	cursor.execute(sql_command)
-	print("...Table created")
+	print("...Table ready")
 
 def insertData(cursor, task):
+	numberOfEntries = getNumberOfEntries(cursor,task)
 	sql_command = """
 		INSERT INTO task (id, title, description) 
 		VALUES ({id}, '{title}', '{description}');
 	"""
-	formatted_sql_command = sql_command.format(id=100, title=task.title, description=task.description)
+	formatted_sql_command = sql_command.format(id=numberOfEntries+1, title=task.title, description=task.description)
 
 	print("Executing command...")
 	print(formatted_sql_command)
 
 	cursor.execute(formatted_sql_command)
+
+def getNumberOfEntries(cursor,task):
+	sql_command = """
+		SELECT COUNT(*) FROM task;
+	"""
+	cursor.execute(sql_command)
+	return cursor.fetchone()[0]
+
 	
 
 	
--- index.py
+++ index.py
@@ -2,9 +2,14 @@ import DbHandler
 import Task
 
 if __name__ == '__main__':
-	name = input("Enter task name: ")
-	description = input("Provide a description: ")
+	while True:
+		name = input("Enter task name: ")
+		description = input("Provide a description: ")
 
-	task = Task.Task(name, description)
-	task.showTask()
-	DbHandler.save(task)
+		task = Task.Task(name, description)
+		task.showTask()
+		DbHandler.save(task)
+
+		decision = input("Add another task? (Enter y for Yes or any other key to quit)")
+		if(decision.lower() != 'y'):
+			break
