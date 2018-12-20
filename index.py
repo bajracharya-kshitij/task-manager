3e85758f30ce4cf0129816724f4c7742fde1c891
Kshitij Bajracharya <bajracharya.kshitij@gmail.com>
2018-12-20 22:42:14 +0545

    Add GUI using tkinter

--- index.py
+++ index.py
@@ -1,15 +1,26 @@
 import DbHandler
 import Task
+from tkinter import *
+
+def createTask():
+	task = Task.Task(name.get(), description.get())
+	task.showTask()
+	DbHandler.save(task)
+	name.delete(0,END)
+	description.delete(0,END)
 
 if __name__ == '__main__':
-	while True:
-		name = input("Enter task name: ")
-		description = input("Provide a description: ")
+	window = Tk()
+	Label(window, text="Name").grid(row=0)
+	Label(window, text="Description").grid(row=1)
 
-		task = Task.Task(name, description)
-		task.showTask()
-		DbHandler.save(task)
+	name = Entry(window)
+	description = Entry(window)
 
-		decision = input("Add another task? (Enter y for Yes or any other key to quit)")
-		if(decision.lower() != 'y'):
-			break
+	name.grid(row=0, column=1)
+	description.grid(row=1, column=1)
+
+	Button(window, text='Save', command=createTask).grid(row=3, column=0, sticky=W, pady=4)
+	Button(window, text='Quit', command=window.quit).grid(row=3, column=1, sticky=W, pady=4)
+
+	mainloop()
